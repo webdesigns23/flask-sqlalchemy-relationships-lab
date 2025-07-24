453a9ef9c97b2f334e950a2b7710538b9faeffac
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-07-24 16:55:58 -0600

    added relationships and routes

--- server/app.py
+++ server/app.py
@@ -16,30 +16,54 @@ db.init_app(app)
 
 # TODO: add functionality to all routes
 
-@app.route('/events')
+@app.route('/events', methods=['GET'])
 def get_events():
-    pass
+    events = Event.query.all()
+    events_list = [{'id': event.id,'name': event.name, 'location': event.location} for event in events]
+    return jsonify(events_list), 200
 
-
-@app.route('/events/<int:id>/sessions')
+@app.route('/events/<int:id>/sessions', methods=['GET'])
 def get_event_sessions(id):
-    pass
+    event = db.session.get(Event, id)
+    if not event:
+        return jsonify({"error": "Event not found"}), 404
+    
+    session_data = [{
+        'id': session.id, 
+        'title': session.title, 
+        'start_time': session.start_time.isoformat()
+        }
+        for session in event.sessions]
+    return jsonify(session_data), 200
 
-
-@app.route('/speakers')
+@app.route('/speakers', methods=['GET'])
 def get_speakers():
-    pass
+    speakers = Speaker.query.all()
+    speakers_list = [{'id': speaker.id,'name': speaker.name} for speaker in speakers]
+    return jsonify(speakers_list), 200
 
-
-@app.route('/speakers/<int:id>')
+@app.route('/speakers/<int:id>', methods=['GET'])
 def get_speaker(id):
-    pass
+    speaker = db.session.get(Speaker, id)
+    if not speaker:
+        return jsonify({"error": "Speaker not found"}), 404
+    bio_text = speaker.bio.bio_text if speaker.bio else "No bio available"
+    response_data = {'id': speaker.id, 'name': speaker.name, 'bio_text': bio_text}
+    return jsonify(response_data), 200
 
-
-@app.route('/sessions/<int:id>/speakers')
+@app.route('/sessions/<int:id>/speakers', methods=['GET'])
 def get_session_speakers(id):
-    pass
-
-
+   session = db.session.get(Session, id)
+   if not session:
+       return jsonify({"error": "Session not found"}), 404
+   
+   speaker_data = [{
+       'id': speaker.id, 
+       'name': speaker.name, 
+       'bio_text': speaker.bio.bio_text if speaker.bio else "No bio available"
+       }
+       for speaker in session.speakers]
+   return jsonify(speaker_data), 200
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -9,7 +9,12 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # TODO: add association table
-
+session_speakers = db.Table('session_speakers', metadata,
+    db.Column('session_id', db.Integer, 
+        db.ForeignKey('sessions.id'), primary_key=True),
+    db.Column('speaker_id', db.Integer,
+        db.ForeignKey('speakers.id'), primary_key=True)
+)
 
 # TODO: set up relationships for all models
 class Event(db.Model):
@@ -18,6 +23,9 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     location = db.Column(db.String, nullable=False)
+
+    # O2M Relationship mapping the event to related sessions
+    sessions = db.relationship('Session', back_populates="event", cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<Event {self.id}, {self.name}, {self.location}>'
@@ -28,8 +36,15 @@ class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     start_time = db.Column(db.DateTime)
-    event_id = db.Column(db.Integer)
+    # O2M add Foreign key stores the Event id
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
+    # M2M Relationship mapping the employee to related meetings
+    speakers = db.relationship(
+        'Speaker', secondary=session_speakers, back_populates='sessions')
+
+    # Relationship mapping the review to related employee
+    event = db.relationship('Event', back_populates="sessions")
 
     def __repr__(self):
         return f'<Session {self.id}, {self.title}, {self.start_time}>'
@@ -41,15 +56,27 @@ class Speaker(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    # M2M Relationship mapping the employee to related meetings
+    sessions = db.relationship(
+        'Session', secondary=session_speakers, back_populates='speakers')
+    
+    # O2O has one bio, Relationship mapping speaker to related bio
+    bio = db.relationship(
+        'Bio', uselist=False, back_populates='speaker')    
+
     def __repr__(self):
-        return f'<Speaker {id}, {name}>'
+        return f'<Speaker {self.id}, {self.name}>'
 
 class Bio(db.Model):
     __tablename__ = 'bios'
 
     id = db.Column(db.Integer, primary_key=True)
     bio_text = db.Column(db.Text, nullable=False)
-    speaker_id = db.Column(db.Integer)
+    # O2O belongs to speaker, add Foreign key to store the speaker id
+    speaker_id = db.Column(db.Integer, db.ForeignKey('speakers.id'))
+
+    # Relationship mapping onboarding to related speaker
+    speaker = db.relationship('Speaker', back_populates='bio')
 
     def __repr__(self):
-        return f'<Bio {id}, {bio_text}>'
+        return f'<Bio {self.id}, {self.bio_text}>'
